99d1ce23c782731bfc4e8a649278495fddb370ee
Yury Korolev <yury.korolev@gmail.com>
2021-01-13 13:35:24 +0300

    Use lib with bitcode

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .binaryTarget(
             name: "libssh2",
             url: "https://github.com/yury/libssh2-apple/releases/download/v1.9.0/libssh2-dynamic.xcframework.zip",
-            checksum: "a0ca3b2bad7a284d338de5f6b466061a37332c8c67938d4c8ac5437ac44c4bc2"
+            checksum: "c08179c8d7d4d97088bc9307289a38d64dcab77ed5c99ff53a8385de76fd3f7d"
         ),
     ]
 )
