be664a9c18f1b6b10b638eb30c37b7c80b6ea4dc
Yury Korolev <yury.korolev@gmail.com>
2021-01-13 15:33:00 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .binaryTarget(
             name: "libssh2",
             url: "https://github.com/yury/libssh2-apple/releases/download/v1.9.0/libssh2-dynamic.xcframework.zip",
-            checksum: "c08179c8d7d4d97088bc9307289a38d64dcab77ed5c99ff53a8385de76fd3f7d"
+            checksum: "325a778eb38476b99de3c1a9007ec7e256d9c12adcc407ed5e271bd9496bf887"
         ),
     ]
 )
